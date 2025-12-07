06e548b2a7ef79889e56768544b46d8e31f1c078
Dhruvi H. Choksi <70427783+DHChoksi@users.noreply.github.com>
2025-12-07 03:09:40 -0800

    Updates

---
+++ Assets/Scripts/Constants.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+namespace Constants {
+    public class Constants
+    {
+
+        public enum SFXType
+        {
+            None,
+            HitEnemy,
+            DeadPlayer,
+            PLayerWin,
+            TreasureSpawn,
+            ClockTick
+        }
+
+        public enum BackgroundMusicType
+        { 
+            LavaFLowing,
+            BackgroundMusic
+        }
+    }
+}
